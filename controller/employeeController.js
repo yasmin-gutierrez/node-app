265bf38f44f46f2af74291bc48be5f791b29d253
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-04-11 13:14:14 -0500

    fetch employees is working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -12,16 +12,21 @@ export const create = async(req, res) => {
 
         const savedEmployee = await employeeData.save();
         res.status(200).json(savedEmployee)
+
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"}) 
     }
 }
 
-
-
 export const fetch = async(req, res) => {
     try {
-        res.json("Hello World")
+        //res.json("Hello World")
+        const employees = await Employee.find()
+        if(employees.length === 0) {
+            return res.status(404).json({message: "Employees not found!"})
+        }
+
+        res.status(200).json(employees)
     } catch (error) { 
         res.status(500).json({error: "Internal Server Error"})
     }
